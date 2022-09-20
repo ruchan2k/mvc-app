bb811484bc0d1dabd412afea1539a29a324c5b5b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-20 12:22:29 +0300

    Modified the view via Controller

--- mvc-app/mvc-app/Controller/Controller.swift
+++ mvc-app/mvc-app/Controller/Controller.swift
@@ -8,9 +8,19 @@
 import UIKit
 
 class Controller: UIViewController {
-
+    
+    @IBOutlet weak var iphoneModelLabel: UILabel!
+    @IBOutlet weak var iphoneColorLabel: UILabel!
+    @IBOutlet weak var iphonePriceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let iphone = AppleProduct(name: "Iphone X", color: "Space Gray", price: 999.99)
+        iphoneModelLabel.text = iphone.name
+        iphoneColorLabel.text = "in \(iphone.color)"
+        iphonePriceLabel.text = String(iphone.price)
         // Do any additional setup after loading the view.
     }
 
