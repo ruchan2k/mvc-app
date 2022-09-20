b9cd3af75ff99229639aa6b06955ab1b76a80a7c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-20 09:22:00 +0300

    Organised files as model-view-controller

---
+++ mvc-app/mvc-app/Controller.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvc-app
+//
+//  Created by Ruçhan Talha Günay on 20.09.2022.
+//
+
+import UIKit
+
+class Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
