3a627ea783e13dd6b83bc0dc02c4edeeb89dd1db
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-20 09:44:04 +0300

    Model AppleProduct is created

---
+++ mvc-app/mvc-app/Model/AppleProduct.swift
@@ -0,0 +1,14 @@
+//
+//  AppleProduct.swift
+//  mvc-app
+//
+//  Created by Ruçhan Talha Günay on 20.09.2022.
+//
+
+import Foundation
+
+struct AppleProduct {
+    var name: String
+    var color: String
+    var price: Double
+}
